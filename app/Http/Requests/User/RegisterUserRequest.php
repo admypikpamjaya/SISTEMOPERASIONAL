e0c6870e6c7e983bc8cb1113fed10e2e00bc2826
ariyapanna <ariyapanna@outlook.com>
2026-01-06 10:13:29 +0700

    feat: Add form request for register and update user

---
+++ app/Http/Requests/User/RegisterUserRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use App\Enums\User\UserRole;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class RegisterUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'username' => 'required|string|unique:users',
+            'role' => ['required', Rule::enum(UserRole::class)]
+        ];
+    }
+
+    public function messages(): array 
+    {
+        return [
+            'name.required' => 'Field nama wajib diisi',
+
+            'username.required' => 'Field username wajib diisi',
+            'username.unique' => 'Username sudah digunakan',
+
+            'role.required' => 'Field role wajib diisi',
+            'role.*' => 'Role tidak valid'
+        ];
+    }
+}
