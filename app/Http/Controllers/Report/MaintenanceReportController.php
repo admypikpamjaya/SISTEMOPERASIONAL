d1d18ef605ea7207b4ec940e0410000d081791d5
ariyapanna <ariyapanna@outlook.com>
2026-01-09 13:06:59 +0700

    feat: Enhance Maintenance Report functionality with CRUD operations and error handling

--- app/Http/Controllers/Report/MaintenanceReportController.php
+++ app/Http/Controllers/Report/MaintenanceReportController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Report;
 
 use App\DTOs\Report\CreateMaintenanceReportDTO;
+use App\DTOs\Report\UpdateMaintenanceReportDTO;
+use App\DTOs\Report\UpdateMaintenanceReportStatusDTO;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Report\CreateMaintenanceReportRequest;
+use App\Http\Requests\Report\UpdateMaintenanceReportRequest;
+use App\Http\Requests\Report\UpdateMaintenanceReportStatusRequest;
 use App\Services\Report\MaintenanceReportService;
 use Illuminate\Http\Request;
 
@@ -13,6 +17,32 @@ class MaintenanceReportController extends Controller
     public function __construct(
         private MaintenanceReportService $service
     ) {}
+
+    public function index(Request $request)
+    {
+        $reports = $this->service->getLogs($request->keyword, $request->page);
+        return view('maintenance-report.index', [
+            'reports' => $reports
+        ]);
+    }
+
+    public function show(string $id)
+    {
+        try 
+        {
+            $report = $this->service->getLog($id);
+            return response()->json([
+                'message' => 'Laporan berhasil ditemukan',
+                'data' => $report
+            ]);
+        }
+        catch(\Throwable $e)
+        {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], $e->getCode() ? $e->getCode() : 500);
+        }
+    }
 
     public function store(CreateMaintenanceReportRequest $request)
     {
@@ -24,6 +54,57 @@ class MaintenanceReportController extends Controller
             return response()->json(['success' => true]);
         }
         catch(\Throwable $e) 
+        {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], $e->getCode() ? $e->getCode() : 500);
+        }
+    }
+
+    public function update(UpdateMaintenanceReportRequest $request)
+    {
+        try 
+        {
+            $this->service->updateLog(UpdateMaintenanceReportDTO::fromArray($request->validated()));
+
+            session()->flash('success', 'Laporan berhasil diperbaharui');
+            return response()->json(['success' => true]);
+        }
+        catch(\Throwable $e)
+        {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], $e->getCode() ? $e->getCode() : 500);
+        }
+    }
+
+    public function updateStatus(UpdateMaintenanceReportStatusRequest $request)
+    {
+        try 
+        {
+            $this->service->updateStatus(UpdateMaintenanceReportStatusDTO::fromArray($request->validated()));
+
+            session()->flash('success', 'Status laporan berhasil diperbaharui');
+            return response()->json(['success' => true]);
+        }
+        catch(\Throwable $e) 
+        {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], $e->getCode() ? $e->getCode() : 500);
+        }
+    }
+
+    public function delete(string $id)
+    {
+        try 
+        {
+            $this->service->deleteLog($id);
+
+            session()->flash('success', 'Laporan berhasil dihapus');
+            return response()->json(['success' => true]);
+        }
+        catch(\Throwable $e)
         {
             return response()->json([
                 'message' => $e->getMessage()
--- app/Services/Report/MaintenanceReportService.php
+++ app/Services/Report/MaintenanceReportService.php
@@ -4,11 +4,38 @@ namespace App\Services\Report;
 
 use App\DTOs\Report\CreateMaintenanceReportDTO;
 use App\DTOs\Report\MaintenanceReportDataDTO;
+use App\DTOs\Report\UpdateMaintenanceReportDTO;
+use App\DTOs\Report\UpdateMaintenanceReportStatusDTO;
+use App\Enums\Report\Maintenance\AssetMaintenanceReportStatus;
 use App\Models\Log\MaintenanceLog;
 use Illuminate\Support\Facades\Log;
 
 class MaintenanceReportService
 {
+    public function getLogs(?string $keyword = null, ?int $page = 1)
+    {
+        $logs = MaintenanceLog::where(function ($query) use ($keyword) {
+                $query->where('worker_name', 'like', "%{$keyword}%")
+                    ->orWhere('issue_description', 'like', "%{$keyword}%")
+                    ->orWhere('working_description', 'like', "%{$keyword}%");
+            })
+            ->orWhereHas('asset', function ($query) use ($keyword) {
+                $query->where('account_code', 'like', "%{$keyword}%");
+            })
+            ->paginate(5, ['*'], 'page', $page);
+
+        return $logs;
+    }
+
+    public function getLog(string $id)
+    {
+        $log = MaintenanceLog::with('asset')->find($id);
+        if(empty($log))
+            throw new \Exception('Laporan tidak ditemukan.', 404);
+
+        return MaintenanceReportDataDTO::fromModel($log);
+    }
+
     public function createLog(CreateMaintenanceReportDTO $dto)
     {
         $log = MaintenanceLog::create([
@@ -19,5 +46,46 @@ class MaintenanceReportService
             'working_description' => $dto->workingDescription
         ])->refresh();
         return MaintenanceReportDataDTO::fromModel($log);
+    }
+
+    public function updateLog(UpdateMaintenanceReportDTO $dto)
+    {
+        $log = MaintenanceLog::find($dto->id);
+        if(empty($log))
+            throw new \Exception('Laporan tidak ditemukan.', 404);
+
+        $log->update([
+            'worker_name' => $dto->workerName,
+            'date' => $dto->workingDate,
+            'issue_description' => $dto->issueDescription,
+            'working_description' => $dto->workingDescription
+        ]);
+
+        return MaintenanceReportDataDTO::fromModel($log);
+    }
+
+    public function updateStatus(UpdateMaintenanceReportStatusDTO $dto)
+    {
+        $log = MaintenanceLog::find($dto->id);
+        if(empty($log))
+            throw new \Exception('Laporan tidak ditemukan.', 404);
+
+        if($dto->status === AssetMaintenanceReportStatus::PENDING)
+            throw new \Exception('Status tidak valid.', 400);
+        
+        $log->update([
+            'status' => $dto->status
+        ]);
+
+        return MaintenanceReportDataDTO::fromModel($log);
+    }
+
+    public function deleteLog(string $id)
+    {
+        $log = MaintenanceLog::find($id);
+        if(empty($log))
+            throw new \Exception('Laporan tidak ditemukan.', 404);
+
+        $log->delete();
     }
 }
