ae45b3ed2c4ccf224156230114b03e363ec46ae5
ProgamingChristo <filmoraxadobe15@gmail.com>
2026-08-19 13:03:11 +0700

    chore: remove trim.php script

--- trim.php
+++
@@ -1,8 +0,0 @@
-<?php
-$file = 'app/Http/Controllers/Admin/BlastRecipientController.php';
-$content = file_get_contents($file);
-$lines = explode(PHP_EOL, $content);
-if(count($lines) < 2) { $lines = explode("\n", $content); }
-$newLines = array_slice($lines, 0, 1674);
-file_put_contents($file, implode(PHP_EOL, $newLines) . PHP_EOL . '}');
-echo "Done\n";
